5ca61e8c343a8bb07f44aa5231004c8f8cfff043
Alejandro <areinac01@iesalbarregas.es>
2023-10-17 12:22:40 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function elementoLyoko() {
+    const element = document.createElement('div');
+    element.className = 'elemento-lyoko';
+
+    //Esto sirve para que la posicion en horizontal en la que salga el elemento sea totalmente aleatoria.
+    element.style.left = `${Math.random() * (window.innerWidth - 50)}px`;
+    document.getElementById('container').appendChild(element);
+
+    setTimeout(() => {
+
+        // Elimina el elemento al caer del todo.
+        element.remove();
+    }, 4000); 
+}
+
+// Crea elementos cada 0,8 segundos aprox.
+setInterval(elementoLyoko, 800); 
